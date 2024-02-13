3b08c33959a6bb8a06fbaebdd239e7d6571e44a2
S567006 <S567006@nwmissouri.edu>
2024-02-13 17:34:57 -0600

    dsda

---
+++ Assingment02-S567006/S567006WhackAMole/S567006WhackAMoleUITests/S567006WhackAMoleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  S567006WhackAMoleUITestsLaunchTests.swift
+//  S567006WhackAMoleUITests
+//
+//  Created by Tarun Yada  on 2/9/24.
+//
+
+import XCTest
+
+final class S567006WhackAMoleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
